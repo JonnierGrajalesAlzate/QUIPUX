6a423d9914b24968bb0b92a8cfc5c6f9015297ce
jONNIER <alzatejonny39@gmail.com>
2023-09-22 16:05:27 -0500

    Probando git bash

---
+++ index.js
@@ -0,0 +1,9 @@
+numero1 = parseInt(prompt("NUMERO1:  "))
+numero2 = parseInt(prompt("NUMERO2:  "))
+numero3 = parseInt(prompt("NUMERO3:  "))
+function Suma(n1,n2,n3){
+    res = n1+n2+n3
+    console.log(`la suma es ${res}`)
+}
+
+Suma(numero1,numero2,numero3)
---
+++ index.py
@@ -0,0 +1,6 @@
+#repasos en git 
+def Suma(*numeross):
+    res = sum(numeross)
+    print(f"la suma es de {res}")
+
+Suma(2,43,56,6)
